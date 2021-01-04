189e7dac945603d6db3346c134d0b1b810a545b6
TegarPenemuan <tegarpenemuan@gmail.com>
2021-01-04 16:07:08 +0700

    first commit

---
+++ TegarPenemuanLibrary/src/main/java/com/tegarpenemuan/tegarpenemuanlibrary/TentangSaya.java
@@ -0,0 +1,14 @@
+package com.tegarpenemuan.tegarpenemuanlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TentangSaya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_saya);
+    }
+}
